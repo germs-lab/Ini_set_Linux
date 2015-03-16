7d6c1b6c4b9a3eae263c65de5b9e8a1408886ef8
Jin Choi <jinchoi721@gmail.com>
2015-03-16 14:25:09 -0500

    first commit

---
+++ PreInstall.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get -y install screen git curl gcc make g++ python-dev unzip default-jre pkg-config libncurses5-dev r-base-core r-cran-gplots python-matplotlib sysstat
+sudo apt-get -y install python-pip ncftp
+sudo pip install screed
